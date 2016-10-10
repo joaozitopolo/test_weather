ae7d5344764a4bbdfe7efc1fac6dd37247c7eeb6
Joao Alberto Melo Polo <joao.polo@metainf.com.br>
2016-10-10 17:10:35 -0300

    load weather cycle using jsonp

--- app/weather.controller.js
+++ app/weather.controller.js
@@ -3,11 +3,11 @@ define(function() {
     /** basic controls for authorization page */
     return function($window, $scope, Weather) {
         var self = this;
-        self.data = {};
-        self.Weather = Weather;
+        //self.data = {};
+        self.data = {"latitude":-29.7549941,"longitude":-51.150283,"status":"OK","coord":{"lon":-51.15,"lat":-29.76},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":293.263,"pressure":1024.1,"humidity":47,"temp_min":293.263,"temp_max":293.263,"sea_level":1032.94,"grnd_level":1024.1},"wind":{"speed":7.03,"deg":102.001},"clouds":{"all":56},"dt":1476129599,"sys":{"message":0.0157,"country":"BR","sunrise":1476089453,"sunset":1476135121},"id":3448622,"name":"Sao Leopoldo","cod":200};
 
         // merges the Weather.data in data field
-        $scope.$watch(function() { return Weather.data; }, function(data) { angular.extend(self.data, data); });
+        $scope.$watch(function() { return Weather.data['status']; }, function(status) { angular.extend(self.data, Weather.data); });
     };
 
 });
--- app/weather.service.js
+++ app/weather.service.js
@@ -2,26 +2,77 @@ define(function(require) {
 
     return function($http) {
         var self = this;
-        var data = {};
-        self.setLocation = function(latitude, longitude) { setLocation(latitude, longitude, $http, self.data); };
+        self.data = {};
+        self.setLocation = function(latitude, longitude) { setLocation(latitude, longitude, self.data, $http); };
     }
 
     /** sets the current location and update the weather */
-    function setLocation(latitude, longitude, $http, data) {
+    function setLocation(latitude, longitude, data, $http) {
         angular.extend(data, { latitude: latitude, longitude: longitude, status: 'PREPARED' });
         loadWeatherByCoordinates(data, $http); 
     }
 
     /** loads the weather for the coordinates  */
     function loadWeatherByCoordinates(data, $http) {
-        $http({ method: 'GET',  
-            url: 'api.openweathermap.org/data/2.5/weather',
-            data: { lat: data.latitude, lon: data.longitude, APPID: 'b2cc0479a61b2a6e145fddca11691478' }
+        var args = "?lat=" + data.latitude + "&lon=" + data.longitude + "&APPID=b2cc0479a61b2a6e145fddca11691478" + "&callback=JSON_CALLBACK";
+        $http({
+            method: 'JSONP',
+            url: 'http://api.openweathermap.org/data/2.5/weather' + args,
         }).then(function(response) {
+            console.log(response);
             angular.extend(data, response.data, { status: 'OK' });
         }, function(response) {
+            console.log(response);
             angular.extend(data, { status: 'ERR', statusText: response.statusText })
         });
     }
 
 });
+
+
+
+/*
+
+{
+    "coord": {
+        "lon": -51.15,
+        "lat": -29.76
+    },
+    "weather": [
+        {
+            "id": 803,
+            "main": "Clouds",
+            "description": "broken clouds",
+            "icon": "04d"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 293.263,
+        "pressure": 1024.1,
+        "humidity": 47,
+        "temp_min": 293.263,
+        "temp_max": 293.263,
+        "sea_level": 1032.94,
+        "grnd_level": 1024.1
+    },
+    "wind": {
+        "speed": 7.03,
+        "deg": 102.001
+    },
+    "clouds": {
+        "all": 56
+    },
+    "dt": 1476128439,
+    "sys": {
+        "message": 0.0029,
+        "country": "BR",
+        "sunrise": 1476089454,
+        "sunset": 1476135120
+    },
+    "id": 3448622,
+    "name": "Sao Leopoldo",
+    "cod": 200
+}
+
+*/
